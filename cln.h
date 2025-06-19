10b32aa4c9aa1e3b55be48624527c61e21fbb6fe
nk0073 <jotaro5jostar@gmail.com>
2025-06-19 05:11:18 -0400

    fixed all parsers, added tests, other minor improvements

--- cln.h
+++ cln.h
@@ -3,10 +3,10 @@
  *
  *  Right now it's only possible to encode the data without layout
  *
- *  TODO: ease the use of arrays
- *  TODO: add tests
+ *  TODO: fix __cln_alloc_get_by_index empty string issues
  *  TODO: make production ready
- *  TODO: add set item function
+ *  TODO: add documentation
+ *  TODO: perhaps add more error codes somewhere
  */
 
 #ifndef _CLN_IMPORTS
@@ -18,7 +18,8 @@
 #define CLN_ERROR_OUT_OF_BOUNDS -1
 #define CLN_ERROR_INVALID_POINTER -2
 #define CLN_ERROR_EMPTY_LAYOUT -3
-#define CLN_ERROR_SNPRINTF -4
+#define CLN_ERROR_EMPTY_STRING -4
+#define CLN_ERROR_SNPRINTF -5
 
 #define CLN_RAW_SEPARATOR ','
 #define CLN_INT64_MAX_LENGTH 22
@@ -115,6 +116,10 @@ cln_buffer cln_create_buffer(size_t bytes) {
 
 void __cln_buffer_increase_capacity(cln_buffer* buffer, const size_t needed_capacity) {
     int8_t increased = 0;
+    if(buffer->capacity == 0 && needed_capacity > 0) {
+        buffer->capacity++;
+    }
+
     while(buffer->capacity < needed_capacity) {
         buffer->capacity *= 2;
         increased = 1;
@@ -148,9 +153,18 @@ int32_t __cln_add_char_ptr(cln_buffer* buffer, const char* str) {
 }
 
 int32_t cln_add_str(cln_buffer* buffer, const char* str, const size_t bytes) {
-    // count the characters that would be formatted as separators
+    if(bytes == 0) {
+        __CLN_ERROR("Empty string passed to cln_add_str");
+        return __cln_add_char_ptr(buffer, "");
+    }
+
+    // count the characters that would be read as separators
     uint32_t unformatted_sep_count = 0;
-    for(const char* c = str + 1; c < str + bytes; ++c) {
+    if(str[0] == CLN_RAW_SEPARATOR) {
+        unformatted_sep_count++;
+    }
+
+    for(const char* c = str; c < str + bytes; ++c) {
         if(c[0] != '\\' && c[1] == CLN_RAW_SEPARATOR) {
             unformatted_sep_count++;
         }
@@ -161,21 +175,20 @@ int32_t cln_add_str(cln_buffer* buffer, const char* str, const size_t bytes) {
     if(unformatted_sep_count > 0) {
         uint32_t len = bytes + unformatted_sep_count;
         char* new_str = malloc(len + 1);
-        *new_str = *str;
-        uint32_t src_str_index = 0;
-        for(uint32_t i = 1; i < len; ++i) {
-            const char* src_c = str + src_str_index;
-            char* c = new_str + i;
-            if(*src_c == CLN_RAW_SEPARATOR && *(src_c - 1) != '\\') {
-                *c = '\\';
-                *(c + 1) = CLN_RAW_SEPARATOR;
-                i++;
+        size_t i = 0;
+        size_t j = 0;
+        for(; j < len; ++i, ++j) {
+            const char* og = str + i;
+            char* n = new_str + j;
+            if(og[0] == CLN_RAW_SEPARATOR && ((i > 0 && og[-1] != '\\') || i == 0)) {
+                n[0] = '\\';
+                n[1] = CLN_RAW_SEPARATOR;
+                j++;
             } else {
-                *c = *src_c;
+                n[0] = og[0];
             }
-
-            src_str_index++;
         }
+
         new_str[len] = '\0';
         exit_code = __cln_add_char_ptr(buffer, new_str);
         free(new_str);
@@ -207,7 +220,7 @@ int32_t __cln_report_snprintf_err(void) {
 
 int32_t cln_add_int(cln_buffer* buffer, int64_t value) {
     char str[CLN_INT64_MAX_LENGTH];
-    if(snprintf(str, CLN_INT64_MAX_LENGTH, "%ld", value) < 0) {
+    if(snprintf(str, CLN_INT64_MAX_LENGTH, "%" SCNd64, value) < 0) {
         return __cln_report_snprintf_err();
     }
 
@@ -216,14 +229,14 @@ int32_t cln_add_int(cln_buffer* buffer, int64_t value) {
 
 int32_t cln_add_uint(cln_buffer* buffer, const uint64_t value) {
     char str[CLN_UINT64_MAX_LENGTH];
-    if(snprintf(str, CLN_UINT64_MAX_LENGTH, "%ld", value) < 0) {
+    if(snprintf(str, CLN_UINT64_MAX_LENGTH, "%" SCNu64, value) < 0) {
         return __cln_report_snprintf_err();
     }
 
     return __cln_add_char_ptr(buffer, str);
 }
 
-// pattern the same that you would use in printf(pattern, value)
+// pattern is the same that you would use in printf(pattern, value)
 int32_t cln_add_float(cln_buffer* buffer, const float value, const char* format) {
     char str[CLN_FLOAT_MAX_LENGTH];
     if(snprintf(str, CLN_FLOAT_MAX_LENGTH, format, value) < 0) {
@@ -268,13 +281,20 @@ int32_t cln_set_layout(cln_buffer* buffer, const cln_layout* layout, const size_
         return CLN_ERROR_EMPTY_LAYOUT;
     }
 
-    free(buffer->layout);
-    free(buffer->allocated_vars);
-
     const size_t layout_bytes = sizeof(cln_layout) * layout_len;
-    buffer->layout = malloc(layout_bytes);
+    const size_t allocator_bytes = sizeof(void*) * layout_len;
+    if(buffer->layout == NULL) {
+        buffer->layout = malloc(layout_bytes);
+    } else {
+        buffer->layout = realloc(buffer->layout, layout_bytes);
+    }
     memcpy(buffer->layout, layout, layout_bytes);
-    buffer->allocated_vars = malloc(sizeof(void*) * layout_len);
+
+    if(buffer->allocated_vars == NULL) {
+        buffer->allocated_vars = malloc(allocator_bytes);
+    } else {
+        buffer->allocated_vars = realloc(buffer->allocated_vars, allocator_bytes);
+    }
 
     buffer->layout_len = layout_len;
     buffer->allocated_len = layout_len;
@@ -286,28 +306,24 @@ int32_t __cln_alloc_get_by_index(cln_buffer* buffer, const size_t index, void** 
     size_t current_index = 0;
     uint32_t start = 0;
     uint32_t len = 0;
-    for(size_t i = 0; i < buffer->size - 1; ++i, ++len) {
-        const char cc = buffer->str[i];     // char current
-        const char cn = buffer->str[i + 1]; // char next
-        if(cc != '\\' && cn == CLN_RAW_SEPARATOR) {
+    for(size_t i = 0; i < buffer->size; ++i, ++len) {
+        const char* c = buffer->str + i;
+        if(c[0] == CLN_RAW_SEPARATOR && ((i > 0 && c[-1] != '\\') || i == 0)) {
             if(current_index == index) {
-                len++;
                 break;
             }
-            current_index++;
-            i += 2;
-            start = i;
+
             len = 0;
+            start = i + 1;
+            current_index++;
         }
     }
 
-    if(len == 0) {
-        *out = NULL;
+    if(len == 0 || (buffer->str + start)[0] == CLN_RAW_SEPARATOR) {
+        *out = malloc(1);
+        strcpy(*out, "");
         return CLN_EMPTY_STRING;
     }
-
-    if(current_index == buffer->layout_len - 1)
-        ++len;
 
     char* str = malloc(len + 1);
     memcpy(str, buffer->str + start, len);
@@ -375,6 +391,7 @@ int32_t __cln_alloc_get_by_index(cln_buffer* buffer, const size_t index, void** 
     return CLN_SUCCESS;
 }
 
+// all va_args should be void** or similar
 int32_t cln_retrieve_items(cln_buffer* buffer, ...) {
     va_list args;
     va_start(args, buffer);
@@ -395,13 +412,16 @@ int32_t cln_retrieve_items(cln_buffer* buffer, ...) {
     return last_result;
 }
 
+// the caller needs to free z themselves
 #define cln_alloc_get_by_index(x, y, z) __cln_alloc_get_by_index(x, y, &z)
 
 void cln_free_buffer(cln_buffer* buffer) {
     free(buffer->str);
     free(buffer->layout);
     for(size_t x = 0; x < buffer->allocated_len; ++x) {
-        free(buffer->allocated_vars[x]);
+        void* __tmp = buffer->allocated_vars[x];
+        free(__tmp);
+        // free(buffer->allocated_vars[x]);
     }
 
     free(buffer->allocated_vars);
--- main.c
+++ main.c
@@ -1,43 +1,175 @@
 #include "cln.h"
-#include <locale.h>
+#include <errno.h>
 #include <stdio.h>
 #include <string.h>
-#define LAYOUT_LEN 4
+
+#define SUCCESS_RETURN(x)                                                                                                             \
+    cln_free_buffer(&x);                                                                                                              \
+    return 0
+
+#define ERROR_RETURN(x)                                                                                                               \
+    cln_free_buffer(&x);                                                                                                              \
+    return 1
+
+// tests cln_add_str, cln_add_int, cln_trim_capacity
+int32_t test_add_buffer(void) {
+    cln_buffer buffer = cln_create_buffer(0);
+    const char* str = ",,wow,,";
+    const int32_t _int = 737373;
+    const int32_t str_r = cln_add_str(&buffer, str, strlen(str));
+    const int32_t int_r = cln_add_int(&buffer, _int);
+    if(str_r < CLN_SUCCESS) {
+        const char* err = cln_get_last_err();
+        printf("test_add_buffer failed\n  str_r error code: %d\n  error message: %s", str_r, err);
+        ERROR_RETURN(buffer);
+    }
+    if(int_r < CLN_SUCCESS) {
+        const char* err = cln_get_last_err();
+        printf("test_add_buffer failed\n  int_r error code: %d\n  error message: %s", int_r, err);
+        ERROR_RETURN(buffer);
+    }
+
+    const char* expected_str = "\\,\\,wow\\,\\,,737373";
+    if(strcmp(buffer.str, expected_str)) {
+        printf("test_add_buffer failed\n  expected string: %s\n  returned stirng: %s\n", expected_str, buffer.str);
+        ERROR_RETURN(buffer);
+    }
+
+    cln_trim_capacity(&buffer);
+    if(strcmp(buffer.str, expected_str)) {
+        printf("test_add_buffer failed\n  before trimming: %s\n  after trimming: %s\n strings do not match", buffer.str, expected_str);
+        ERROR_RETURN(buffer);
+    }
+
+    SUCCESS_RETURN(buffer);
+}
+
+// test cln_add_uint, cln_add_double, cln_add_str for an empty string, cln_set_layout, cln_retrieve_items
+int32_t test_layout(void) {
+#define __TEST_D_FORMAT "%.17f"
+    cln_buffer buffer = cln_create_buffer(12);
+
+    const uint8_t _uint = 66;
+    const char* empty_str = "";
+    const double _double = 412897124984.89247981249;
+
+    const int32_t uint_r = cln_add_uint(&buffer, _uint);
+    const int32_t empty_str_r = cln_add_str(&buffer, empty_str, strlen(empty_str));
+    const int32_t double_r = cln_add_double(&buffer, _double, __TEST_D_FORMAT);
+
+    if(uint_r < CLN_SUCCESS) {
+        const char* err = cln_get_last_err();
+        printf("test_layout failed\n  uint_r error code: %d\n  error message: %s\n", uint_r, err);
+        ERROR_RETURN(buffer);
+    }
+    if(empty_str_r < CLN_SUCCESS && empty_str_r != CLN_ERROR_EMPTY_STRING) {
+        const char* err = cln_get_last_err();
+        printf("test_layout failed\n  empty_str_r error code: %d\n  error message: %s\n", empty_str_r, err);
+        ERROR_RETURN(buffer);
+    }
+    if(double_r < CLN_SUCCESS) {
+        const char* err = cln_get_last_err();
+        printf("test_layout failed\n  double_r error code: %d\n  error message: %s\n", double_r, err);
+        ERROR_RETURN(buffer);
+    }
+
+    const size_t layout_len = 3;
+    const cln_layout layout[] = {CLN_UINT8, CLN_STRING, CLN_DOUBLE};
+    const int32_t set_layout_r = cln_set_layout(&buffer, layout, layout_len);
+    if(set_layout_r < CLN_SUCCESS) {
+        const char* err = cln_get_last_err();
+        printf("test_layout failed\n  set_layout_r error code: %d\n  error message: %s\n", set_layout_r, err);
+        ERROR_RETURN(buffer);
+    }
+    
+    uint8_t* read_uint = NULL;
+    char* read_empty_str = NULL;
+    double* read_double = NULL;
+    int32_t retrieve_items_r = cln_retrieve_items(&buffer, &read_uint, &read_empty_str, &read_double);
+    if(retrieve_items_r < CLN_SUCCESS) {
+        //TODO: change to this when retrieve_items will write to cln_last_error
+        // const char* err = cln_get_last_err();
+        // printf("test_layout1 failed\n  retrieve_items_r error code: %d\n  error message: %s\n", retrieve_items_r, err);
+        // ERROR_RETURN(buffer);
+        
+        printf("test_layout failed\n  retrieve_items_r error code: %d\n\n", retrieve_items_r);
+        ERROR_RETURN(buffer);
+    }
+
+    if(*read_uint != _uint) {
+        printf("test_layout failed\n  *read_uint: %d\n  _uint: %d\n\n", *read_uint, _uint);
+        ERROR_RETURN(buffer);
+    }
+    if(strcmp(read_empty_str, empty_str)) {
+        printf("test_layout failed\n  read_empty_str doesn't match empty_str\n\n");
+        ERROR_RETURN(buffer);
+    }
+    if(*read_double != _double) {
+        printf("test_layout failed\n  *read_double: " __TEST_D_FORMAT "\n  _double: " __TEST_D_FORMAT "\n\n", *read_double, _double);
+        ERROR_RETURN(buffer);
+    }
+
+    SUCCESS_RETURN(buffer);
+}
+
+// tests cln_read_buffer, cln_retrieve_items
+int32_t test_read_buffer(void) {
+#define __TEST_STR1 "frivolous\\,\\,!!!!"
+#define __TEST_INT16 82
+#define __TEST_STR2 "73"
+    const char* cln_source = "frivolous\\,\\,!!!!,82,73";
+    const size_t layout_len = 3;
+    const cln_layout layout[] = {CLN_STRING, CLN_INT16, CLN_STRING};
+    cln_buffer buffer = cln_read_buffer(cln_source);
+    const int32_t set_layout_r = cln_set_layout(&buffer, layout, layout_len);
+    if(set_layout_r < CLN_SUCCESS) {
+        const char* err = cln_get_last_err();
+        printf("test_read_buffer failed\n  set_layout_r error code: %d\n  error message: %s\n", set_layout_r, err);
+        ERROR_RETURN(buffer);
+    }
+
+    char* read_str1 = NULL;
+    int16_t* read_int = NULL;
+    char* read_str2 = NULL;
+    int32_t retrieve_items_r = cln_retrieve_items(&buffer, &read_str1, &read_int, &read_str2);
+    if(retrieve_items_r < CLN_SUCCESS) {
+        //TODO: change to this when retrieve_items will write to cln_last_error
+        // const char* err = cln_get_last_err();
+        // printf("test_layout1 failed\n  retrieve_items_r error code: %d\n  error message: %s\n", retrieve_items_r, err);
+        // ERROR_RETURN(buffer);
+        
+        printf("test_read_buffer failed\n  retrieve_items_r error code: %d\n\n", retrieve_items_r);
+        ERROR_RETURN(buffer);
+    }
+
+    if(strcmp(read_str1, __TEST_STR1)) {
+        printf("test_readbuffer failed\n strings don't match\n  read_str1: %s\n  str1: %s\n\n", read_str1, __TEST_STR1);
+        ERROR_RETURN(buffer);
+    }
+    if(*read_int != __TEST_INT16) {
+        printf("test_readbuffer failed\n  *read_int: %d\n  _int: %d\n\n", *read_int, __TEST_INT16);
+        ERROR_RETURN(buffer);
+    }
+    if(strcmp(read_str2, __TEST_STR2)) {
+        printf("test_readbuffer failed\n strings don't match\n  read_str2: %s\n  str2: %s\n\n", read_str2, __TEST_STR2);
+        ERROR_RETURN(buffer);
+    }
+
+    SUCCESS_RETURN(buffer);
+}
 
 int main(void) {
-    cln_buffer buffer = cln_create_buffer(3);
-    const char* s_str = "hello!";
-    const int32_t s_int = 32;
-    const double s_double = 17249812489.127491247819;
-    const char* s_estr = "";
-    cln_add_str(&buffer, s_str, strlen(s_str));
-    cln_add_int(&buffer, s_int);
-    cln_add_double(&buffer, s_double, "%f");
-    cln_add_str(&buffer, s_estr, strlen(s_estr));
-    const cln_layout x[LAYOUT_LEN] = {CLN_STRING, CLN_INT32, CLN_DOUBLE, CLN_STRING};
-    cln_set_layout(&buffer, x, LAYOUT_LEN);
-    printf("src str: |%s|\nsrc int: |%d|\nsrc double: |%f|\n", s_str, s_int, s_double);
+    uint32_t failed_count = 0;
+    failed_count += test_add_buffer();
+    failed_count += test_layout();
+    failed_count += test_read_buffer();
 
-    char* r_str = NULL;
-    int32_t* r_int = NULL;
-    double* r_double = NULL;
-    char* r_estr = NULL;
-    cln_retrieve_items(&buffer, &r_str, &r_int, &r_double, &r_estr);
-    printf("res str: |%s|\nres int: |%d|\nres double: |%f|\n", r_str, *r_int, *r_double);
-    printf("\nstr equal: %d\nint equal: %d\ndouble equal: %d\n", strcmp(s_str, r_str) == 0, s_int == *r_int, s_double == *r_double);
-    printf("res empty str: |%s|\n", r_estr);
+    if(failed_count == 0) {
+        printf("\033[32mAll tests were successful.\033[0m\n");
+    } else {
+        printf("\033[31m%d of the tests failed.\033[0m\n", failed_count);
+        return 1;
+    }
 
-    cln_free_buffer(&buffer);
-
-    const char* sample = "55.3,very good";
-    cln_buffer read_buffer = cln_read_buffer(sample);
-    cln_set_layout(&read_buffer, (cln_layout[]){CLN_FLOAT, CLN_STRING}, 2);
-
-    float* rr_float = NULL;
-    char* rr_str = NULL;
-    cln_retrieve_items(&read_buffer, &rr_float, &rr_str);
-    printf("\n\nread buffer success: %d\n", *rr_float == 55.3f && strcmp(rr_str, "very good") == 0);
-
-    cln_free_buffer(&read_buffer);
     return 0;
 }
